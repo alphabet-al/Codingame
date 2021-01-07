fcd695b1262d7c63d12df7c1ecc03c2803c9ff3f
Alan Vanichtheeranont <alphabet.al135@gmail.com>
2021-01-07 17:08:10 -0600

    1d done

---
+++ 1D_Spreadsheet/solution.py
@@ -0,0 +1,53 @@
+def do_operation(idx, grp):
+    operation, arg_1, arg_2 = grp
+    idx_error = False
+
+
+
+    if '$' in arg_1 and isinstance(lst[int(arg_1[1:])], int):
+        arg_1 = lst[int(arg_1[1:])]
+    elif '$' in arg_1 and not isinstance(lst[int(arg_1[1:])], int):
+        idx_error = True
+      
+    if '$' in arg_2 and isinstance(lst[int(arg_2[1:])], int):
+        arg_2 = lst[int(arg_2[1:])]
+    elif '$' in arg_2 and not isinstance(lst[int(arg_2[1:])], int):
+        idx_error = True
+
+
+    if not idx_error:
+        if operation == 'ADD':
+            lst[idx] = (int(arg_1) + int(arg_2))
+        elif operation == 'SUB':
+            lst[idx] = (int(arg_1) - int(arg_2))
+        elif operation == 'VALUE':
+            lst[idx] = (int(arg_1))
+        elif operation == 'MULT':
+            lst[idx] = (int(arg_1) * int(arg_2))
+
+
+
+
+if __name__ == "__main__":
+    n = 2 # int(input())
+    ipt = ['ADD $1 20', 'VALUE 32 _']
+    lst = [ipt[i].split() for i in range(n)]
+    
+
+    while True:
+        intcount = 0
+
+        for i in range(len(lst)):
+
+            if not isinstance(lst[i], int):
+                do_operation(i,lst[i])
+     
+            else:
+                intcount += 1
+        
+        if intcount == n:
+            break
+
+    for i in lst:
+        print(i)
+        
